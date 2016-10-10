d4fd27148fce09a35f06a0c8624f709307d8b3c4
Heather Stone <heather.blair.stone@gmail.com>
2016-10-09 22:55:48 -0600

    add illustration, more content, some jquery

--- js/app.js
+++ js/app.js
@@ -8,23 +8,18 @@ function config($stateProvider, $urlRouterProvider) {
  $stateProvider
  .state('home', {
    url: '/',
-  //  controller: 'HomeController',
    templateUrl: 'home.html'
  }).state('home.code', {
    url: 'code',
-  //  controller: 'CodeController',
    templateUrl: 'html/code.html'
  }).state('home.illustrations', {
    url: 'illustrations',
-  //  controller: 'IllustrationsController',
    templateUrl: 'html/illustrations.html'
  }).state('home.science', {
    url: 'science',
-  //  controller: 'ScienceController',
    templateUrl: 'html/science.html'
  }).state('home.writing', {
    url: 'writing',
-  //  controller: 'WritingController',
    templateUrl: 'html/writing.html'
  })
 }
--- js/query.js
+++ js/query.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
+  clickContact();
+})
+
+function clickContact(){
   $('#contact').click(function(){
     $('.social-icons').toggle();
-  })
-})
+    $("html, body").animate({ scrollTop: $(document).height() }, "slow");
+    return false;
+  });
+}
