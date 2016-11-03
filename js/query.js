641e48c7a4c759ae620705f727c17e789d95cab2
Heather Stone <heather.blair.stone@gmail.com>
2016-11-02 22:14:06 -0600

    add botanize video

--- js/query.js
+++ js/query.js
@@ -5,7 +5,9 @@ $(document).ready(function(){
 function clickContact(){
   $('#contact').click(function(){
     $('.social-icons').toggle();
-    $("html, body").animate({ scrollTop: $(document).height() }, "slow");
+    $("html, body").animate({
+      scrollTop: $(document).height()
+    }, "slow");
     return false;
   });
 }
